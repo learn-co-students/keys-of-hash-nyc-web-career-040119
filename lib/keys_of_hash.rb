24c408cb655eb43c2eac52d563628e3aee4702c7
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-24 21:14:08 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+  output = []
+  arguments.each do |args|
+  #binding.pry
+  self.each do | animal , location |
+    if location == args
+      output  << animal
+    end 
   end
-end
+  end # end of args each
+  #binding.pry
+  output
+  end  # end of method keys_of 
+end # end of class
